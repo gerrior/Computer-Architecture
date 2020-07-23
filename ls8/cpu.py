b08732397190961631f92312524b67adb6ec9cf1
Mark Gerrior <github@gerrior.com>
2020-07-23 13:17:30 -0700

    Step 10: Implement System Stack

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,11 +3,13 @@
 import sys
 
 instructions = {
-    '???': 0b00000000,
-    'HLT': 0b00000001,
-    'LDI': 0b00000010,
-    'PRN': 0b00000111,
-    'MUL': 0b00100010
+    '???':  0b00000000,
+    'HLT':  0b00000001,
+    'LDI':  0b00000010,
+    'PUSH': 0b00000101,
+    'POP':  0b00000110,
+    'PRN':  0b00000111,
+    'MUL':  0b00100010
 }
 
 
@@ -17,18 +19,22 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 0xFF  # x is a list of 25 zeroes
-        self.registers = [0] * 0x0F
+        self.registers = [0] * 0x08
         self.pc = 0     # Program Counter, address of the currently executing instruction
         self.ir = 0     # Instruction Register, contains a copy of the currently executing instruction
         self.mar = 0    # Memory Address Register, holds the memory address we're reading or writing
         self.mdr = 0    # Memory Data Register, holds the value to write or the value just read
         self.fl = 0     # Flags, see below
 
+        self.sp = 0xF4
+
         self.branch_table = {}
-        self.branch_table[instructions['HLT']] = self.hlt
-        self.branch_table[instructions['LDI']] = self.ldi
-        self.branch_table[instructions['PRN']] = self.prn
-        self.branch_table[instructions['MUL']] = self.mul
+        self.branch_table[instructions['HLT' ]] = self.hlt
+        self.branch_table[instructions['LDI' ]] = self.ldi
+        self.branch_table[instructions['PUSH']] = self.push
+        self.branch_table[instructions['POP' ]] = self.pop
+        self.branch_table[instructions['PRN' ]] = self.prn
+        self.branch_table[instructions['MUL' ]] = self.mul
 
     # Register getter methods
     @property
@@ -53,7 +59,7 @@ class CPU:
     def r6(self):
         return self.registers[6]
     @property
-    def r7(self):
+    def sp(self):
         return self.registers[7]
 
     # Register setter methods
@@ -78,8 +84,8 @@ class CPU:
     @r6.setter
     def r6(self, value):
         self.registers[6] = value
-    @r7.setter
-    def r7(self, value):
+    @sp.setter
+    def sp(self, value):
         self.registers[7] = value
 
     def ram_read(self, mar):
@@ -102,6 +108,7 @@ class CPU:
             filename = sys.argv[1] # This will throw if there is no second argument
             # filename = "examples/mult.ls8"
             # filename = "examples/print8.ls8"
+            # filename = "examples/stack.ls8"
 
             file = open(filename, "r")
             for line in file:
@@ -120,7 +127,7 @@ class CPU:
             print(f"{filename} did not contain a program")
             sys.exit()
         except:
-            if len(filename) == 0:
+            if filename != None and len(filename) == 0:
                 print("Second argument must be filename: python3 ls8.py examples/print.ls8")
             else:
                 print(f"Unable to open filename {filename}")
@@ -181,6 +188,33 @@ class CPU:
     def ldi(self):
         self.registers[self.ram_read(self.pc + 1) & 0x07] = self.ram_read(self.pc + 2)
         return
+
+    def push(self):
+        # Decrement stack pointer
+        self.sp -= 1
+
+        # Keep R7 in the range 00-FF
+        self.sp &= 0xFF
+
+        # get register value
+        reg_num = self.ram_read(self.pc + 1)
+        value = self.registers[reg_num]
+
+        # Store in memory
+        address_to_push_to = self.sp
+        self.ram_write(address_to_push_to, value)
+
+    def pop(self):
+        # Get value from RAM
+        address_to_pop_from = self.sp
+        value = self.ram_read(address_to_pop_from)
+
+        # Store in the given register
+        reg_num = self.ram_read(self.pc + 1)
+        self.registers[reg_num] = value
+
+        # Increment SP
+        self.sp += 1
 
     def prn(self):
         print(self.registers[self.ram_read(self.pc + 1) & 0x07])
